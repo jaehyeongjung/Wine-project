1b5a418acb8991b54cb42d053833fd0cab0bf4e8
송형진 <sori4606@naver.com>
2025-02-06 15:28:27 +0900

    feat: 기기 감지를 위한 useDevice 훅 구현 및 header, landingpage 구현#10

---
+++ components/layout/Header.tsx
@@ -0,0 +1,46 @@
+import styles from './Header.module.css';
+import useDevice from '../../hooks/useDevice';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+const Header = () => {
+  const { mode } = useDevice();
+  const router = useRouter();
+
+  if (!mode) return null;
+
+  const handleLogoClick = () => {
+    if (router.pathname === '/') {
+      router.reload();
+    } else {
+      router.push('/');
+    }
+  };
+
+  return (
+    <header
+      className={`${styles.header_container} ${styles[`header_container_${mode}`]}`}
+    >
+      <nav className={`${styles.header_box} ${styles[`header_box${mode}`]}`}>
+        <button
+          onClick={handleLogoClick}
+          className={`${styles.wine_logo} ${styles[`logo_${mode}`]}`}
+        >
+          WINE
+        </button>
+        <nav
+          className={`${styles.sign_container} ${styles[`sign_container_${mode}`]}`}
+        >
+          <Link href="/login" className={styles.signin}>
+            로그인
+          </Link>
+          <Link href="/signup" className={styles.signup}>
+            회원가입
+          </Link>
+        </nav>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
