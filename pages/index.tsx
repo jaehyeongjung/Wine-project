cd9497d66be28f640623ce7a346f74a1849f395d
woodfriday <uyj5371@naver.com>
2025-02-06 16:06:04 +0900

    feat: 공통 input 제작 #9

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,16 @@
 import { NextPage } from 'next';
-import { useState } from 'react';
 import Head from 'next/head';
-import Modal from './components/common/Modal';
-import Test from './components/layout/Test';
-import Button from './components/common/Button';
-import styles from '@/styles/Home.module.css';
 
 const Home: NextPage = () => {
-  const [showModal, setShowModal] = useState(false);
-
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Button Component Test</title>
+        <title>Wine Project</title>
+        <meta name="description" content="A description of the wine project" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <h1>첫 페이지</h1>
-      <div>
-        <Button
-          onClick={() => setShowModal(true)}
-          type="default"
-          size="width96x42"
-          text="테스트"
-          color="purple"
-          textColor="white"
-        />
-        <Modal
-          className={styles.modalBox}
-          showModal={showModal}
-          closeModal={() => setShowModal(false)}
-          clossBtn={true}
-        >
-          <Test />
-        </Modal>
-      </div>
     </div>
   );
 };
