f7c9954df03932ae01e6a03d932c2ffef60dacc5
kimsoyoung <sy_busay@naver.com>
2025-02-06 14:20:44 +0900

    feat: 모달창 기능 및 기본 css 구현 #12

---
+++ pages/components/layout/Test.tsx
@@ -0,0 +1,10 @@
+const Test = () => {
+  return (
+    <div>
+      <h2>test modal222</h2>
+      <p>components</p>
+    </div>
+  );
+};
+
+export default Test;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,87 +1,38 @@
 import { NextPage } from 'next';
+import { useState } from 'react';
 import Head from 'next/head';
-import Button from '@/pages/components/common/Button';
+import Modal from './components/common/Modal';
+import Test from './components/layout/Test';
+import Button from './components/common/Button';
 import styles from '@/styles/Home.module.css';
 
 const Home: NextPage = () => {
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Button Component Test</title>
       </Head>
-
-      <h1>버튼 컴포넌트 테스트</h1>
-
-      <Button
-        type="default"
-        size="width400"
-        text="가입하기"
-        color="purple"
-        textColor="white"
-      />
-      <Button
-        type="default"
-        size="width279"
-        text="와인 보러가기"
-        color="purple"
-        textColor="white"
-      />
-      <Button
-        type="default"
-        size="width284"
-        text="와인 등록하기"
-        color="purple"
-        textColor="white"
-      />
-      <Button
-        type="default"
-        size="width96x42"
-        text="초기화"
-        color="lite_purple"
-        textColor="purple"
-      />
-      <Button
-        type="default"
-        size="width223"
-        text="필터 적용하기"
-        color="purple"
-        textColor="white"
-      />
-
-      <Button
-        type="default"
-        size="width280"
-        text="280"
-        color="purple"
-        textColor="white"
-      />
-
-      <Button
-        type="default"
-        size="width108"
-        text="확인"
-        color="purple"
-        textColor="white"
-      />
-
-      <Button
-        type="default"
-        size="width113"
-        text="113"
-        color="purple"
-        textColor="white"
-      />
-
-      <Button
-        type="default"
-        size="width480"
-        text="리뷰 남기기"
-        color="purple"
-        textColor="white"
-      />
-
-      <Button type="kakao" size="social" text="kakao로 시작하기" />
-      <Button type="google" size="social" text="Google로 시작하기" />
+      <h1>첫 페이지</h1>
+      <div>
+        <Button
+          onClick={() => setShowModal(true)}
+          type="default"
+          size="width96x42"
+          text="테스트"
+          color="purple"
+          textColor="white"
+        />
+        <Modal
+          className={styles.modalBox}
+          showModal={showModal}
+          closeModal={() => setShowModal(false)}
+          clossBtn={true}
+        >
+          <Test />
+        </Modal>
+      </div>
     </div>
   );
 };
