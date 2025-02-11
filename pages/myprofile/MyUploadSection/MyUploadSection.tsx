79b006a0ac226f8ca99d91f46a5d21a972e0b7f2
jaehyeongjung <titochung625@naver.com>
2025-02-11 14:50:58 +0900

    chore: myprofile-page css 규격 맞춤

--- pages/myprofile/MyUploadSection/MyUploadSection.tsx
+++ pages/myprofile/MyUploadSection/MyUploadSection.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import styles from './MyUploadSection.module.css';
+import { symbol } from 'zod';
 const MyUploadSection: React.FC = () => {
   const [selectedTab, setSelectedTab] = useState<'comment' | 'wine'>('comment');
 
@@ -34,11 +35,76 @@ const MyUploadSection: React.FC = () => {
       </div>
       {selectedTab === 'comment' ? (
         <div className={styles.myComment}>
-          <div className={styles.myCommentMap}></div>
+          <div className={styles.myCommentMap}>
+            <div className={styles.myCommentInner}>
+              <div className={styles.myCommentInnerStarAndTimeContainer}>
+                <div className={styles.myCommentInnerStarAndTime}>
+                  <div className={styles.myCommentInnerStar}>
+                    <div className={styles.myCommentInnerStarImg}>
+                      <img src="/icons/starColor.svg"></img>
+                    </div>
+                    <p
+                      className={`${styles.myCommentInnerStarNum} text-2lg-bold`}
+                    >
+                      5.0
+                    </p>
+                  </div>
+                  <p className={`${styles.myCommentInnerTime} text-lg-regular`}>
+                    10시간전
+                  </p>
+                </div>
+                <div className={styles.myCommentInnerDropdown}>
+                  <img src="/icons/dropDown.svg"></img>
+                </div>
+              </div>
+              <div className={styles.myCommentInnerDeatail}>
+                <p
+                  className={`${styles.myCommentInnerDetailTitle} text-lg-medium`}
+                >
+                  Sentinal Carbernet Sauvignon 2016
+                </p>
+                <p className={styles.myCommentInnerDetailReview}>
+                  Deep maroon color, tasting notes of blackberry, dark
+                  chocolate, plum. Super jammy and bold with some smoky after
+                  notes. Big flavor. Amazing value (would pay three times the
+                  price for it), well balanced flavor. Could drink all day
+                  everyday with or without food. I need more immediately.
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       ) : (
-        <div className={styles.Registerwine}>
-          <div className={styles.RegisterwineMap}></div>
+        <div className={styles.RegisterwineContainer}>
+          <div className={styles.Registerwine}>
+            <div className={styles.RegisterwineMap}>
+              <div className={styles.RegisterwineImg}>
+                <img src="/images/testWine.svg"></img>
+              </div>
+              <div className={styles.RegisterwineData}>
+                <div className={styles.RegisterwineDataText}>
+                  <div className={styles.RegisterwineDataTextDetail}>
+                    <div className={styles.RegisterwineDataTextDetailTitle}>
+                      Sentinel Carbernet Sauvignon 2016
+                    </div>
+                    <div
+                      className={`${styles.RegisterwineDataTextDetailFrom} text-lg-regular`}
+                    >
+                      Western Cape, South Africa
+                    </div>
+                  </div>
+                  <div className={styles.RegisterwineDataTextDropdown}>
+                    <img src="/icons/dropDown.svg"></img>
+                  </div>
+                </div>
+                <div
+                  className={`${styles.RegisterwineDataPrice} text-2lg-bold`}
+                >
+                  ₩ 64,990
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       )}
     </div>
