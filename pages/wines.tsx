ffa14f9f14db7d8f991e0b1f06894a2f6bfe31b9
jaehyeongjung <titochung625@naver.com>
2025-02-12 15:37:57 +0900

    refactor: useDevice 훅을 이용한 wines.tsx 반응형 셋팅

--- pages/wines.tsx
+++ pages/wines.tsx
@@ -3,8 +3,10 @@ import styles from './wines.module.css';
 import Header from '@/components/layout/Header';
 import Input from '@/components/common/Input';
 import StarRating from '@/components/common/StarRating';
+import useDevice from '@/hooks/useDevice';
 
 const Wines: React.FC = () => {
+  const { mode } = useDevice();
   const wineList = [
     {
       id: 1,
@@ -33,23 +35,49 @@ const Wines: React.FC = () => {
   return (
     <>
       <Header />
-      <div className={styles.winesContainer}>
-        <div className={styles.winesRecommend}>
-          <p className={styles.winesRecommendText}>이번 달 추천 와인</p>
-          <div className={styles.wineRecommendSlide}>
-            <div className={styles.wineRecommendSlideMap}>
-              <div className={styles.wineRecoomnedSlideInner}>
-                <div className={styles.wineRecommendSlideImg}>
+      <div
+        className={`${styles.winesContainer} ${styles[`winesContainer_${mode}`]}`}
+      >
+        <div
+          className={`${styles.winesRecommend} ${styles[`winesRecommend_${mode}`]}`}
+        >
+          <p
+            className={`${styles.winesRecommendText} ${styles[`winesRecommendText_${mode}`]}`}
+          >
+            이번 달 추천 와인
+          </p>
+          <div
+            className={`${styles.winesRecommendSlide} ${styles[`winesRecommendSlide_${mode}`]}`}
+          >
+            <div
+              className={`${styles.winesRecommendSlideMap} ${styles[`winesRecommendSlideMap_${mode}`]}`}
+            >
+              <div
+                className={`${styles.winesRecommendSlideInner} ${styles[`winesRecommendSlideInner_${mode}`]}`}
+              >
+                <div
+                  className={`${styles.winesRecommendSlideImg} ${styles[`winesRecommendSlideImg_${mode}`]}`}
+                >
                   <img src="/images/testWine.svg" alt="와인 이미지" />
                 </div>
-                <div className={styles.wineRecommendSlideData}>
-                  <div className={styles.RecommendContentRating}>
-                    <p className={styles.RecommnedContentRaitingNumber}>4.8</p>
-                    <div className={styles.starRatingContiner}>
+                <div
+                  className={`${styles.winesRecommendSlideData} ${styles[`winesRecommendSlideData_${mode}`]}`}
+                >
+                  <div
+                    className={`${styles.RecommendContentRating} ${styles[`RecommendContentRating_${mode}`]}`}
+                  >
+                    <p
+                      className={`${styles.RecommnedContentRaitingNumber} ${styles[`RecommendContentRatingNumber_${mode}`]}`}
+                    >
+                      4.8
+                    </p>
+                    <div
+                      className={`${styles.starRatingContainer} ${styles[`starRatingContainer_${mode}`]}`}
+                    >
                       <StarRating rating={4.8} size={12.84} />
                     </div>
                     <p
-                      className={`${styles.RecommendContentRatingText} text-xs-regular`}
+                      className={`${styles.RecommendContentRatingText} ${styles[`RecommendContentRatingText_${mode}`]} text-xs-regular`}
                     >
                       Sentinel Carbernet Sauvignon 2016
                     </p>
@@ -59,7 +87,9 @@ const Wines: React.FC = () => {
             </div>
           </div>
         </div>
-        <div className={styles.custom_input_wrapper}>
+        <div
+          className={`${styles.custom_input_wrapper} ${styles[`custom_input_wrapper_${mode}`]}`}
+        >
           <Input
             type="search"
             icon="search"
@@ -67,61 +97,92 @@ const Wines: React.FC = () => {
             size="search"
           />
         </div>
-        <div className={styles.wines_list}>
-          <div className={styles.wines_listMapContainer}>
+        <div className={`${styles.wines_list} ${styles[`wines_list_${mode}`]}`}>
+          <div
+            className={`${styles.wines_listMapContainer} ${styles[`wines_listMapContainer_${mode}`]}`}
+          >
             {wineList.map((wine) => (
-              <div key={wine.id} className={styles.wines_listMap}>
-                <div className={styles.wines_lisMapDetailContainer}>
-                  <div className={styles.wines_listMapDetail}>
-                    <div className={styles.wines_listMapDetailImg}>
+              <div
+                key={wine.id}
+                className={`${styles.wines_listMap} ${styles[`wines_listMapContainer_${mode}`]}`}
+              >
+                <div
+                  className={`${styles.wines_listMapDetailContainer} ${styles[`wines_listMapDetailContainer_${mode}`]}`}
+                >
+                  <div
+                    className={`${styles.wines_listMapDetail} ${styles[`wines_listMapDetail_${mode}`]}`}
+                  >
+                    <div
+                      className={`${styles.wines_listMapDetailImg} ${styles[`wines_listMapDetailImg_${mode}`]}`}
+                    >
                       <img src={wine.image} alt="와인 이미지" />
                     </div>
-                    <div className={styles.wines_listMapDetailContnet}>
-                      <div className={styles.wines_listMapDetailContentData}>
-                        <div className={styles.ContentTitleAndFrom}>
+                    <div
+                      className={`${styles.wines_listMapDetailContent} ${styles[`wines_listMapDetailContent_${mode}`]}`}
+                    >
+                      <div
+                        className={`${styles.wines_listMapDetailContentData} ${styles[`wines_listMapDetailContentData_${mode}`]}`}
+                      >
+                        <div
+                          className={`${styles.ContentTitleAndFrom} ${styles[`ContentTitleAndFrom_${mode}`]}`}
+                        >
                           <div
-                            className={`${styles.ContentTitle} text-3xl-semibold`}
+                            className={`${styles.ContentTitle} ${styles[`ContentTitle_${mode}`]} text-3xl-semibold`}
                           >
                             {wine.name}
                           </div>
                           <div
-                            className={`${styles.ContentFrom} text-lg-regular`}
+                            className={`${styles.ContentFrom} ${styles[`ContentFrom_${mode}`]} text-lg-regular`}
                           >
                             {wine.origin}
                           </div>
                         </div>
-                        <div className={styles.ContentRating}>
-                          <p className={styles.ContentRatingNumber}>
+                        <div
+                          className={`${styles.ContentRating} ${styles[`ContentRaing_${mode}`]}`}
+                        >
+                          <p
+                            className={`${styles.ContentRatingNumber} ${styles[`ContentRaingNumber_${mode}`]}`}
+                          >
                             {wine.rating}
                           </p>
                           <StarRating rating={wine.rating} size={17.12} />
                           <p
-                            className={`${styles.ContentRatingTotal} text-md-regular`}
+                            className={`${styles.ContentRatingTotal} ${styles[`ContentRatingTotal_${mode}`]} text-md-regular`}
                           >
                             {wine.reviewCount}개 후기
                           </p>
                         </div>
                       </div>
-                      <div className={styles.wines_listMapDetailContentPrice}>
-                        <div className={`${styles.ContentPrice} text-xl-bold`}>
+                      <div
+                        className={`${styles.wines_listMapDetailContentPrice} ${styles[`wines_listMapDetailContentPrice_${mode}`]}`}
+                      >
+                        <div
+                          className={`${styles.ContentPrice} ${styles[`ContentPrice_${mode}`]} text-xl-bold`}
+                        >
                           ₩ {wine.price.toLocaleString()}
                         </div>
-                        <div className={styles.ContentPriceBtn}>
+                        <div
+                          className={`${styles.ContentPriceBtn} ${styles[`ContentPriceBtn_${mode}`]}`}
+                        >
                           <img src="/icons/priceBtn.svg" alt="가격 버튼" />
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
-                <div className={styles.wines_lisMapReviewContianer}>
-                  <div className={styles.wines_listMapReview}>
+                <div
+                  className={`${styles.wines_listMapReviewContianer} ${styles[`wines_listMapReviewContainer_${mode}`]}`}
+                >
+                  <div
+                    className={`${styles.wines_listMapReview} ${styles[`wines_listMapReview_${mode}`]}`}
+                  >
                     <p
-                      className={`${styles.wines_listMapReviewTitle} text-lg-semibold`}
+                      className={`${styles.wines_listMapReviewTitle} ${styles[`wines_listMapReviewTitle`]} text-lg-semibold`}
                     >
                       최신후기
                     </p>
                     <p
-                      className={`${styles.wines_listMapReviewContent} text-lg-regular`}
+                      className={`${styles.wines_listMapReviewContent} ${styles[`wines_listMapReviewContent`]} text-lg-regular`}
                     >
                       {wine.review}
                     </p>
